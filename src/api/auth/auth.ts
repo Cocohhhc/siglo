9cd082c01e0d42ce2c73be8d20507c757e05cbd7
Cocohhhc <estevenpaula0123@gmail.com>
2025-12-08 07:45:50 -1100

    Modificando archivo Auth y modificando dependencias

--- src/api/auth/auth.ts
+++ src/api/auth/auth.ts
@@ -1,22 +1,13 @@
 "use server";
 
-interface LoginData {
+interface PacientData {
   gmail: string;
-  password: string;
-}
-
-interface RegisterData {
-  name: string;
-  lastName: string;
-  IdNumber: string;
-  mail: string;
   password: string;
   departamentId: string;
 }
 
-
-// LOGIN
-export async function loginAction(data: LoginData): Promise<void> {
+// Datos de paciente
+export async function loginAction(data: PacientData): Promise<void> {
   const res = await fetch("https://mi-backend.loca.It/auth/login", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
@@ -29,52 +20,29 @@ export async function loginAction(data: LoginData): Promise<void> {
   }
 }
 
-// REGISTER
-export async function registerAction(data: RegisterData): Promise<void> {
-  const res = await fetch("https://mi-backend.loca.It/auth/register", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(data),
-    credentials: "include",
-  });
-
-  if (!res.ok) {
-    throw new Error("Error al registrarse");
-  }
-}
 
 //Obtienes los datos del formulario
 
 export async function getData(formData: FormData): Promise<void> {
-  const userName = (formData.get("userName") ?? "").toString().trim();
-  const userLastName = (formData.get("userLastName") ?? "").toString().trim();
-  const userCardId = (formData.get("userCardId") ?? "").toString().trim();
-  const userPassword = (formData.get("userPassword") ?? "").toString().trim();
-  const userGmail = (formData.get("userGmail") ?? "").toString().trim();
-  const departamentId = (formData.get("departamentId") ?? "").toString().trim();
+  const pacientName = (formData.get("pacienteName") ?? "").toString().trim();
+  const pacientLastName = (formData.get("pacientLastName") ?? "").toString().trim();
+  const pacientId = (formData.get("pacientId") ?? "").toString().trim();
+  const dataOfBirth = (formData.get("dataOfBirth") ?? "").toString().trim();
+  const yearsOld = (formData.get("yearsOld") ?? "").toString().trim();
+  const date = (formData.get("date") ?? "").toString().trim();
 
   const values = {
-    userName,
-    userLastName,
-    userCardId,
-    userPassword,
-    userGmail,
-    departamentId,
+    pacientName,
+    pacientLastName,
+    pacientId,
+    dataOfBirth,
+    yearsOld,
+    date
   };
 
-  // LOGIN
-  await loginAction({
-    gmail: userGmail,
-    password: userPassword,
-  });
-
-  // REGISTER
-  await registerAction({
-    name: userName,
-    lastName: userLastName,
-    IdNumber: userCardId,
-    mail: userGmail,
-    password: userPassword,
-    departamentId,
-  });
+  // // LOGIN
+  // await loginAction({
+  //   gmail: userGmail,
+  //   password: userPassword,
+  // });
 }
