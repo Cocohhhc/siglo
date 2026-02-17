0d34591d8762025a3932022494e30025a2fb2ee8
wandyDev <wandydev01@gmail.com>
2026-02-17 15:55:38 -0400

    Refactor del tipado entrega controller

--- src/modules/entrega/entrega.controller.ts
+++ src/modules/entrega/entrega.controller.ts
@@ -1,48 +1,49 @@
 import { Controller, Get, Param, Post } from '@nestjs/common';
 import { EntregaService } from './entrega.service';
+import { entrega } from '@prisma/client';
 
 @Controller('entrega')
 export class EntregaController {
   constructor(private readonly entregaService: EntregaService) {}
   //find all entrega by user id
   @Get('/recibidas/:userId')
-  findAll(@Param('userId') userId: string) {
+  findAll(@Param('userId') userId: string): Promise<entrega[]> {
     return this.entregaService.findAll(+userId);
   }
 
   //find all entrega enviadas por el usuario
   @Get('/enviadas/:userId')
-  findAllEnviadas(@Param('userId') userId: string) {
+  findAllEnviadas(@Param('userId') userId: string): Promise<entrega[]> {
     return this.entregaService.findAllEnviados(+userId);
   }
 
   //find all entrega aceptadas por el usuario
   @Get('/aceptadas/:userId')
-  findAceptados(@Param('userId') userId: string) {
+  findAceptados(@Param('userId') userId: string): Promise<entrega[]> {
     return this.entregaService.findAceptados(+userId);
   }
 
   //find all entrega rechazadas por el usuario
   @Get('/rechazadas/:userId')
-  findRechazados(@Param('userId') userId: string) {
+  findRechazados(@Param('userId') userId: string): Promise<entrega[]> {
     return this.entregaService.findRechazados(+userId);
   }
 
   //find one entrega by id
   @Get('findOne/:id')
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id') id: string): Promise<entrega> {
     return this.entregaService.findOne(+id);
   }
 
   //rechazar entrega
   @Post('/rechazar/:id')
-  rechazar(@Param('id') id: string) {
+  rechazar(@Param('id') id: string): Promise<entrega> {
     return this.entregaService.rechazar(+id);
   }
 
   //aceptar entrega
   @Post('/aceptar/:id')
-  aceptar(@Param('id') id: string) {
+  aceptar(@Param('id') id: string): Promise<entrega> {
     return this.entregaService.aceptar(+id);
   }
 }
